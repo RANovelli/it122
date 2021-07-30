7874b2a31ba57323051ed5d85a41c63d09916920
ryana <ryan.a.novelli@gmail.com>
2021-07-30 16:03:48 -0700

    models Uploads completed HW 5 assignment

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 import express from 'express';
 import exphbs from "express-handlebars";
 import { Band } from "./models/bands.js";
+import cors from 'cors';
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.set('port', process.env.PORT || 3000);
 app.use(express.static('./public')); // set location for static files
 app.use(express.urlencoded()); //Parse URL-encoded bodies
 app.use(express.json()); // Used to parse JSON bodies
+app.use('/api', cors()); // set Access-Control-Allow-Origin header for api route
 
 app.engine("handlebars", exphbs({defaultLayout: "main"}));
 app.set("view engine", "handlebars");
@@ -57,6 +59,40 @@ app.get('/detail', (req,res,next) => {
 //     let found = data.getItem(req.body.name);
 //     res.render("details", {band: req.body.name, result: found, band: data.getAll()});
 // });
+
+// API routes
+app.get('/api/bands', (req,res) => {
+    Band.find({}).lean()
+        .then((bands) => {
+            res.json(bands)
+        })
+        .catch(err => next(err));
+});
+
+app.get('/api/bands/detail/:name', (req,res,next) => {
+    Band.findOne({ name:req.params.name }).lean()
+        .then((band) => {
+            res.json(band)
+        })
+        .catch(err => next(err));
+});
+
+app.post('/api/bands/add', (req,res,next) => {
+    Band.updateOne({'name':req.body.name}, req.body, {upsert:true}, (err, result) => {
+        if (err) return next(err);
+        console.log(result);
+        // .catch(err => next(err));
+        res.json({"message": "band added"})    
+    });
+});    
+
+app.get('/api/bands/delete/:name', (req,res) => {
+    Band.deleteOne({ name:req.params.name }, (err, result) => {
+        if (err) return next(err);
+        console.log(result)
+        res.json({"message": "delete successful"})    
+    });
+});
 
 // define 404 handler
 app.use((req,res) => {
